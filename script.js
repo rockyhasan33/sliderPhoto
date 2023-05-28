7cce415afc7616fc3177be99412ab4a092a46bec
rockybul <rockyhasan403@gmail.com>
2023-05-28 17:06:52 +0530

    fixed the bugs

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const prevBtn = document.querySelector('.prev');
 const nextBtn = document.querySelector('.next');
 const display = document.querySelector('.images');
-console.log(display)
+
 let num = 1;
 
 nextBtn.addEventListener('click', nexDisplay);
@@ -11,10 +11,10 @@ function nexDisplay() {
     display.animate([{opacity:'0.1'}, {opacity:'1.0'}]
     ,{duration: 1000,fill:'forwards'})
     num++;
-    if(num >= 5) {
+    if(num > 5) {
         num = 1;
     }
-    console.log(num)
+
     display.style.background = `url(./images/image${num}.jpg)center/cover no-repeat`;
 
     
@@ -29,7 +29,6 @@ function preDisplay() {
     if(num < 1) {
         num = 5;
     }
-    console.log(num)
     display.style.background = `url(./images/image${num}.jpg)center/cover no-repeat`;
 
     
